91f28df4bb9e761fd8d28442c1b7922e450f259b
Shachar Ifrach <33417983+shaharyi@users.noreply.github.com>
2024-02-25 10:36:45 +0200

    Update isSeq.java

--- src/main/java/isSeq.java
+++ src/main/java/isSeq.java
@@ -1,13 +1,12 @@
 	public static boolean isSeq(Queue<Integer> q, int x) {
 		boolean flag = false;
 		q.insert(null);
-		Integer back = q.head();
 		while(q.head() != null) {
+			int back = q.remove();
+			q.insert(back);	
 			if(back == q.head() && back == x) {
 				flag = true;
 			}
-			back = q.remove();
-			q.insert(back);	
 		}
 		return flag;
 	}
